bd939dba5acc885022dffcaa69859ad88846cbc4
freecodecampvideo <freecodecampvideo@gmail.com>
2022-01-24 19:06:08 -0600

    first commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0xeBC1C63139F5Ee9F67DEf2E4902A84C86ce27eF7";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
